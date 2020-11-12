db8f8e8ae3db42670ab14bcae4fab34dfe2ae4ea
Xiaoming Li <lixiaoming1993@hotmail.com>
2020-11-12 15:33:17 +0800

    Homework #1: Convert Timer to a stateless component

--- ch11/timer/timer.jsx
+++ ch11/timer/timer.jsx
@@ -1,12 +1,21 @@
-class Timer extends React.Component {
-  render() {
-    if (this.props.timeLeft == 0) {
-      document.getElementById('end-of-time').play()
-    }
-    if (this.props.timeLeft == null || this.props.timeLeft == 0)
-      return <div/>
-    return <h1>Time left: {this.props.timeLeft}</h1>
+// class Timer extends React.Component {
+//   render() {
+//     if (this.props.timeLeft == 0) {
+//       document.getElementById('end-of-time').play()
+//     }
+//     if (this.props.timeLeft == null || this.props.timeLeft == 0)
+//       return <div/>
+//     return <h1>Time left: {this.props.timeLeft}</h1>
+//   }
+// }
+
+const Timer = () => {
+  if (this.props.timeLeft == 0) {
+    document.getElementById('end-of-time').play()
   }
+  if (this.props.timeLeft == null || this.props.timeLeft == 0)
+    return <div/>
+  return <h1>Time left: {this.props.timeLeft}</h1>
 }
 
 class Button extends React.Component {
